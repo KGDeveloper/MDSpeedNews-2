1dc2da085b8a2b42c72fa2e83bef7f028d629c4b
Medalands <medalands@172-6-0-101.lightspeed.irvnca.sbcglobal.net>
2016-04-06 09:53:11 +0800

    Initial Commit

---
+++ MDSpeedNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MDSpeedNews
+//
+//  Created by Medalands on 16/4/6.
+//  Copyright © 2016年 Medalands. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
